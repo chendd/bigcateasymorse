4d10e430ccd5ed197f09722c143f5e0f3c11bf02
jiessiedyh@gmail.com <jiessiedyh@gmail.com@7c148b2a-f2c3-11de-9a6a-9d1f184cc70b>
2010-09-08 13:37:25 +0000

    根据ip地址确定地理位置

---
+++ ip_region/src/main/java/util/RegionModel.java
@@ -0,0 +1,45 @@
+package util;
+
+public class RegionModel {
+
+	private String country;
+	
+	private String city;
+	// 经度
+	private String longitude;
+	// 纬度
+	private String latitude;
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+}
