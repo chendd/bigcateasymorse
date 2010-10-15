336e49d5dc629c007cd1e143b75dbbcfba1e507c
jiessiedyh@gmail.com <jiessiedyh@gmail.com@7c148b2a-f2c3-11de-9a6a-9d1f184cc70b>
2010-10-15 02:30:31 +0000

    android notification 实例

---
+++ android_notification/src/com/easymorse/notification/ReceivedNotificationActivity.java
@@ -0,0 +1,33 @@
+package com.easymorse.notification;
+
+import org.apache.http.util.LangUtils;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+public class ReceivedNotificationActivity extends Activity {
+	Intent intent;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.main);
+		intent = this.getIntent();
+		Log.v("sign>>>>",
+				"------------" + intent.getExtras().getString("height"));
+
+	}
+}
