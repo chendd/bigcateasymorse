ead6c611c9a067074f6ab9949de4c6b19fedc066
jiessiedyh@gmail.com <jiessiedyh@gmail.com@7c148b2a-f2c3-11de-9a6a-9d1f184cc70b>
2010-10-17 17:13:21 +0000

    实现了更为复杂的listview布局

---
+++ androidlistviewdemo0.4/src/com/easymorse/ListVIewDate.java
@@ -0,0 +1,88 @@
+package com.easymorse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListVIewDate {
+	public static  List<Map<String, Object>> getData() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		map.put("title", "雪豹");
+		map.put("info", "简介: 　电视剧《雪豹》从时间上贯穿了整个抗日战争时期。");
+		map.put("img", R.drawable.xue);
+
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "士兵突击");
+		map.put("info", "简介: 青山绿水之间，人们日出而作，日落而息。");
+		map.put("img", R.drawable.shi);
+
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+
+		map.put("title", "雪豹");
+		map.put("info", "简介: 　电视剧《雪豹》从时间上贯穿了整个抗日战争时期。");
+		map.put("img", R.drawable.xue);
+
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "士兵突击");
+		map.put("info", "简介: 青山绿水之间，人们日出而作，日落而息。");
+		map.put("img", R.drawable.shi);
+
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+
+		map.put("title", "雪豹");
+		map.put("info", "简介: 　电视剧《雪豹》从时间上贯穿了整个抗日战争时期。");
+		map.put("img", R.drawable.xue);
+
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "士兵突击");
+		map.put("info", "简介: 青山绿水之间，人们日出而作，日落而息。");
+		map.put("img", R.drawable.shi);
+
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+
+		map.put("title", "雪豹");
+		map.put("info", "简介: 　电视剧《雪豹》从时间上贯穿了整个抗日战争时期。");
+		map.put("img", R.drawable.xue);
+
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "士兵突击");
+		map.put("info", "简介: 青山绿水之间，人们日出而作，日落而息。");
+		map.put("img", R.drawable.shi);
+
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+
+		map.put("title", "雪豹");
+		map.put("info", "简介: 　电视剧《雪豹》从时间上贯穿了整个抗日战争时期。");
+		map.put("img", R.drawable.xue);
+
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "士兵突击");
+		map.put("info", "简介: 青山绿水之间，人们日出而作，日落而息。");
+		map.put("img", R.drawable.shi);
+
+		list.add(map);
+		return list;
+	}
+}
