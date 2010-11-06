5d3140a30ba0b6d95f2ce23b31df2216e20320d3
jiessiedyh@gmail.com <jiessiedyh@gmail.com@7c148b2a-f2c3-11de-9a6a-9d1f184cc70b>
2010-11-06 09:48:37 +0000

    对书签增加了删除，新增功能。

--- androidbookmark1.1/src/com/mybm/MainActivity.java
+++ androidbookmark1.1/src/com/mybm/MainActivity.java
@@ -5,11 +5,12 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Browser;
 import android.provider.Browser.BookmarkColumns;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,13 +22,16 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		insertBookMark();
+		deleteBookMark();
 		listView = (ListView) findViewById(R.id.bookmarklist);
 		listView.setAdapter(new ArrayAdapter<String>(this,
 				android.R.layout.simple_expandable_list_item_1, getData()));
+
 	}
 
 	private List<String> getData() {
-		
+
 		List<String> data = new ArrayList<String>();
 		ContentResolver contentResolver = getContentResolver();
 		Cursor cursor = contentResolver.query(Browser.BOOKMARKS_URI, null,
@@ -45,4 +49,34 @@ public class MainActivity extends Activity {
 		return data;
 	}
 
+	private void insertBookMark() {
+		ContentValues inputValue = new ContentValues();
+		// Bookmark值为1
+		inputValue.put(Browser.BookmarkColumns.BOOKMARK, 1);
+		// 添加书签Title
+		inputValue.put(Browser.BookmarkColumns.TITLE, "witmob");
+		// 添加书签URL
+		inputValue.put(Browser.BookmarkColumns.URL,
+				"http://bigcat.easymorse.com/");
+
+		ContentResolver contentResolver = getContentResolver();
+
+		// 向浏览器添加该书签
+
+		contentResolver.insert(Browser.BOOKMARKS_URI, inputValue);
+
+	}
+
+	private void deleteBookMark() {
+
+		ContentResolver contentResolver = getContentResolver();
+
+		// 删除书签
+
+		int number = contentResolver.delete(Browser.BOOKMARKS_URI,
+				Browser.BookmarkColumns.TITLE + "=? and "
+						+ Browser.BookmarkColumns.URL + "=?", new String[] {
+						"witmob", "http://bigcat.easymorse.com/" });
+
+	}
 }
