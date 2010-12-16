392b522eb26d93c49680e06e6c5b036518973c9c
jiessiedyh@gmail.com <jiessiedyh@gmail.com@7c148b2a-f2c3-11de-9a6a-9d1f184cc70b>
2010-12-16 17:40:33 +0000

    android 恢复出厂 demo

---
+++ androidreset1.0/src/com/reset/DeviceAdminSample.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.reset;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+/**
+ * Example of a do-nothing admin class. When enabled, it lets you control some
+ * of its policy and reports when there is interesting activity.
+ */
+public class DeviceAdminSample extends DeviceAdminReceiver {
+
+	static SharedPreferences getSamplePreferences(Context context) {
+		return context.getSharedPreferences(
+				DeviceAdminReceiver.class.getName(), 0);
+	}
+
+	void showToast(Context context, CharSequence msg) {
+		Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onEnabled(Context context, Intent intent) {
+		showToast(context, "激活");
+	}
+
+	@Override
+	public CharSequence onDisableRequested(Context context, Intent intent) {
+		return "确定要取消激活";
+	}
+
+	@Override
+	public void onDisabled(Context context, Intent intent) {
+		showToast(context, "取消激活");
+	}
+
+}
