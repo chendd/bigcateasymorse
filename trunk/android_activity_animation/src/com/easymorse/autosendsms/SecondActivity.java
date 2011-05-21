98311bd075797d0d75a05d34259f95e926d05e51
jiessiedyh@gmail.com <jiessiedyh@gmail.com@7c148b2a-f2c3-11de-9a6a-9d1f184cc70b>
2011-05-21 07:13:04 +0000

    android activity切换动画

---
+++ trunk/android_activity_animation/src/com/easymorse/autosendsms/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.easymorse.autosendsms;
+
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+
+
+
+	}
+
+	
+}
