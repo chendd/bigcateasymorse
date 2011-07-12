d790431ee74913f0bc8394a9c0557aa9ea629153
jiessiedyh@gmail.com <jiessiedyh@gmail.com@7c148b2a-f2c3-11de-9a6a-9d1f184cc70b>
2011-07-12 16:30:35 +0000

    修改了listview的布局

--- trunk/android-listview-separate-demo0.2/src/com/easymorse/separate/MainActivity.java
+++ trunk/android-listview-separate-demo0.2/src/com/easymorse/separate/MainActivity.java
@@ -5,19 +5,24 @@ package com.easymorse.separate;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
 	ListView listView;
 	
 	MyAdapter myAdapter;
+	
+	public List<String> listTag = new ArrayList<String>();
+
 	
     /** Called when the activity is first created. */
     @Override
@@ -34,50 +39,74 @@ public class MainActivity extends Activity {
 		List<String> data = new ArrayList<String>();
 		int i = 0;
 
-		data.add("------------" + (i++));
+		data.add("A");
+		listTag.add("A");
 		data.add("aa试数据" + (i++));
 		data.add("a试数据" + (i++));
 		data.add("aa试数据" + (i++));
-		data.add("------------" + (i++));
+		listTag.add("B");
+		data.add("B");
 		data.add("bb试数据" + (i++));
 		data.add("b试数据" + (i++));
 		data.add("b试数据" + (i++));		
 		data.add("b试数据" + (i++));
-		data.add("------------" + (i++));
+		listTag.add("C");
+		data.add("C");
 		data.add("c测试数据" + (i++));
 		data.add("c测试数据" + (i++));
-		data.add("------------" + (i++));
+		listTag.add("D");
+		data.add("D");
 		data.add("d测试数据" + (i++));
 		data.add("d测试数据" + (i++));
 		data.add("d测试数据" + (i++));
-		data.add("------------" + (i++));
+		listTag.add("E");
+		data.add("E");
 		data.add("e测试数据" + (i++));
 		data.add("e测试数据" + (i++));
 		data.add("e测试数据" + (i++));
-		data.add("--------" + (i++));
+		listTag.add("F");
+		data.add("F" );
 		data.add("f测试数据" + (i++));
 		return data;
 	}
-}
-class MyAdapter extends ArrayAdapter<String> {
-
-	public MyAdapter(Context context,  int textViewResourceId,
-			List<String> objects) {
-		super(context,  textViewResourceId, objects);
 	
-	}
+	class MyAdapter extends ArrayAdapter<String> {
 
-	@Override
-	public boolean areAllItemsEnabled() {
-		return false; 
-		// 不是所有项都可选
-	}
+		public MyAdapter(Context context,  int textViewResourceId,
+				List<String> objects) {
+			super(context,  textViewResourceId, objects);
+		
+		}
 
-	@Override
-	public boolean isEnabled(int position) {
-		return !getItem(position).startsWith("-"); 
-		// 如果-开头，则该项不可选
+		@Override
+		public boolean areAllItemsEnabled() {
+			return false; 
+			// 不是所有项都可选
+		}
+
+		@Override
+		public boolean isEnabled(int position) {
+			return !listTag.contains(getItem(position)); 
+			// 如果-开头，则该项不可选
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+		    View view = convertView;
+		    //根据标签类型加载不通的布局模板
+		    if(listTag.contains(getItem(position))){
+		        //如果是标签项
+		        view = LayoutInflater.from(getContext()).inflate(R.layout.group_list_item_tag, null);
+		    }else{              
+		        //否则就是数据项了      
+		        view = LayoutInflater.from(getContext()).inflate(R.layout.group_list_item, null);
+		    }
+		    //显示名称
+		    TextView textView = (TextView) view.findViewById(R.id.group_list_item_text);
+		    textView.setText(getItem(position));
+		    //返回重写的view
+		    return view;
+		}
+		
 	}
-	
-	
 }
